8a7e1179d20a8c373c4a65a97ce1855fea8d24ea
Eldorbek94 <eldorbtc@gmail.com>
2024-10-30 17:14:51 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Calculate days of years \n Input: ");
+
+int year = Convert.ToInt32(Console.ReadLine());
+int dif = 2023 - year;
+int days_dif = dif*365;
+Console.WriteLine($"Output: {days_dif}");
